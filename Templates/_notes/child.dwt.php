e00122efb81fd993c912f252425d41a43b8c608e
Phạm Nguyễn Nguyệt Anh <54847547+nguyetanh1999@users.noreply.github.com>
2020-10-15 17:47:11 +0700

    Add files via upload

---
+++ Templates/_notes/child.dwt.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html><head>
+<meta charset="utf-8">
+
+<!-- TemplateBeginEditable name="EditRegion1" -->
+
+<title>Quản lý Hồ sơ Sinh viên</title>
+
+<!-- TemplateEndEditable -->
+
+<link rel="icon" type="image/png" href="../images/icons/1.ico"/>
+<link rel="stylesheet" type="text/css" href="../css/index.css"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href='https://fonts.googleapis.com/css?family=Raleway:400,500,300' rel='stylesheet' type='text/css'>
+<script src='https://kit.fontawesome.com/a076d05399.js'></script>
+
+<!-- TemplateBeginEditable name="EditRegion2" -->
+
+<!-- TemplateEndEditable -->
+
+</head>
+<body>
+<div class="all">
+	<div class="lisbanner">
+    	<div class="lislogo">
+      		<a href="../index.php" title="Trang chủ"><img id="lislogoimg" src="../images/LIS_COLOR.png"/></a>
+    		<p style="font-family:Logo; font-size:60px; margin-top:30px; color:#003D59">LIS</br></br></br></br></br></p>
+            <ul>
+              <li><a href="../p/about.html">Giới thiệu</a></li>
+              <li><a href="../p/guide.html">Hướng dẫn</a></li>
+              <li><a href="../p/contact.html">Liên hệ</a></li>
+            </ul>
+    	</div>
+<!-- TemplateBeginEditable name="EditRegion3" -->       
+
+
+<!-- TemplateEndEditable -->
+
+ 
+  	
+    </div>
+    
+<!-- TemplateBeginEditable name="EditRegion4" -->
+<img src="../images/ava.png"/>
+<!-- TemplateEndEditable -->
+
+
+
+</div>
+    <div class="footer">
+    </br></br>Đại học Quốc gia Thành phố Hồ Chí Minh</br>
+    Trường Đại học Khoa học Xã hội và Nhân văn</br>
+    Khoa Thư viện - Thông tin học
+    </div>
+</body>
+</html>
---
+++ Templates/_notes/index.dwt.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html><head>
+<meta charset="utf-8">
+<!-- TemplateBeginEditable name="EditRegion1" -->
+<title>Quản lý Hồ sơ Sinh viên</title>
+<!-- TemplateEndEditable -->
+<link rel="icon" type="image/png" href="images/icons/1.ico"/>
+<link rel="stylesheet" type="text/css" href="css/index.css"/>
+<link href='https://fonts.googleapis.com/css?family=Raleway:400,500,300' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" href="../fontawesome-free-5.12.0-web/css/all.min.css"/>
+<!-- TemplateBeginEditable name="EditRegion2" -->
+<!-- TemplateEndEditable -->
+</head>
+<body>
+<div class="all">
+	<div class="lisbanner">
+    	<div class="lislogo">
+      		<img id="lislogoimg" src="images/LIS_COLOR.png"/>
+    		<p style="font-family:Logo; font-size:60px; margin-top:30px; color:#003D59">LIS</br></br></br></br></br></p>
+            <ul>
+              <li><a href="p/about.html">Giới thiệu</a></li>
+              <li><a href="p/guide.html">Hướng dẫn</a></li>
+              <li><a href="p/contact.html">Liên hệ</a></li>
+            </ul>
+    	</div>
+        
+<!-- TemplateBeginEditable name="EditRegion4" -->
+<!-- TemplateEndEditable -->  
+  	
+    </div>
+    
+<!-- TemplateBeginEditable name="EditRegion5" -->
+<!-- TemplateEndEditable -->  
+
+</div>
+    <div class="footer">
+    </br></br>Đại học Quốc gia Thành phố Hồ Chí Minh</br>
+    Trường Đại học Khoa học Xã hội và Nhân văn</br>
+    Khoa Thư viện - Thông tin học
+    </div>
+</body>
+</html>
